8a4322283ad60e03aa8a697d9da1a3b3c09d1434
bplindgren <bplindgren91@gmail.com>
2016-08-08 09:33:09 -0500

    updates

--- public/js/application.js
+++ public/js/application.js
@@ -1,21 +1,21 @@
-$(document).ready(function({
+// $(document).ready(function({
 
-  $('#standings-type').on('submit', function(event) {
-    event.preventDefault();
+//   $('#standings-type').on('submit', function(event) {
+//     event.preventDefault();
 
-    var $route = $this.attr('action')
-    var $data = $this.serialize()
-    var $request = $.ajax({
-      method: 'GET',
-      url: $route,
-      data: $data,
-    });
+//     var $route = $this.attr('action')
+//     var $data = $this.serialize()
+//     var $request = $.ajax({
+//       method: 'GET',
+//       url: $route,
+//       data: $data,
+//     });
 
-    $request.done(function(partial) {
-      $('#full-league-standings-table').append(partial);
-      console.log(partial)
-    });
-  });
+//     $request.done(function(partial) {
+//       $('#full-league-standings-table').append(partial);
+//       console.log(partial)
+//     });
+//   });
 
-})
+// })
 
