e68f389dce98c211279ed365489a7b6843369876
bplindgren <bplindgren91@gmail.com>
2016-09-07 14:39:11 -0500

    Fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718001803) do
+ActiveRecord::Schema.define(version: 20160907193730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,10 +22,10 @@ ActiveRecord::Schema.define(version: 20160718001803) do
     t.float    "away_score",   null: false
     t.integer  "home_team_id", null: false
     t.float    "home_score",   null: false
-    t.boolean  "completed?",   null: false
     t.string   "game_type",    null: false
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.boolean  "completed?"
     t.index ["away_team_id"], name: "index_games_on_away_team_id", using: :btree
     t.index ["home_team_id"], name: "index_games_on_home_team_id", using: :btree
   end
