5a0b8b3516055ce0f1cf37da166696985d79eacb
bplindgren <bplindgren91@gmail.com>
2016-09-09 12:50:09 -0500

    small fixes

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -4,6 +4,7 @@ class LeaguesController < ApplicationController
     @league = League.find(params[:id])
     @teams = @league.teams
     @seasons = @league.seasons
+    @first_five = Game.where(completed?: false).first(5)
   end
 
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -11,28 +11,29 @@ class Team < ApplicationRecord
   validates :season_id, :name, :abbr, :owner_id, :division, presence: true
 
   def team_stats(game_type)
-    games = games_by_type(game_type)
     stats = {:wins => 0, :losses => 0, :ties => 0, :points_for => 0, :points_against => 0, :point_diff => 0, :ppg => 0, :papg => 0, :ppg_diff => 0, :wp => 0.000}
 
     games.each do |game|
-      scores = [game.home_score, game.away_score].sort.reverse
-      if game.winner == nil
-        stats[:ties] += 1
-        break if game == "tie"
-      elsif game.winner.owner_id == self.owner.id
-        stats[:wins] += 1
-        stats[:points_for] += scores[0]
-        stats[:points_against] += scores[1]
-      elsif game.loser.owner_id == self.owner.id
-        stats[:losses] += 1
-        stats[:points_for] += scores[1]
-        stats[:points_against] += scores[0]
+      if game.completed?
+        scores = [game.home_score, game.away_score].sort.reverse
+        if game.winner == nil
+          stats[:ties] += 1
+          break if game == "tie"
+        elsif game.winner.owner_id == self.owner.id
+          stats[:wins] += 1
+          stats[:points_for] += scores[0]
+          stats[:points_against] += scores[1]
+        elsif game.loser.owner_id == self.owner.id
+          stats[:losses] += 1
+          stats[:points_for] += scores[1]
+          stats[:points_against] += scores[0]
+        end
+        stats[:point_diff] = stats[:points_for] - stats[:points_against]
+        stats[:ppg] = stats[:points_for] / games.count
+        stats[:papg] = stats[:points_against] / games.count
+        stats[:ppg_diff] = stats[:ppg] - stats[:papg]
+        stats[:wp] = stats[:wins].to_f / games.count
       end
-      stats[:point_diff] = stats[:points_for] - stats[:points_against]
-      stats[:ppg] = stats[:points_for] / games.count
-      stats[:papg] = stats[:points_against] / games.count
-      stats[:ppg_diff] = stats[:ppg] - stats[:papg]
-      stats[:wp] = stats[:wins].to_f / games.count
     end
     stats
   end
